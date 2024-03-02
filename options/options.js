1c0c600bd1fe3bc6b58bf6747bc2e524b2fb6e93
Crowdbotics <team@crowdbotics.com>
2024-03-02 02:48:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-recipe-46766",
+  url: "https://royal-recipe-46766.botics.co",
+  api: "https://royal-recipe-46766.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
